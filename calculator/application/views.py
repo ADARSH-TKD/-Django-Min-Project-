d85f5e5c0ce16b260258569c7911cb9c817817d4
ADARSH-TKD <162183918+ADARSH-TKD@users.noreply.github.com>
2024-12-23 15:20:21 +0530

    calculator is ready 0.1

--- calculator/application/views.py
+++ calculator/application/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpRenspose
-# Create your views here.
-def index(request):
-    return render("index.html")
+
+def calculator(request):
+    c = ""
+    if request.method == "POST":
+        n1 = request.POST.get("num1")
+        n2 = request.POST.get("num2")  
+        opr = request.POST.get("opr")
+
+        # Validate inputs
+        if n1.isdigit() and n2.isdigit():
+            n1 = int(n1)
+            n2 = int(n2)
+
+            if opr == "+":
+                c = n1 + n2
+            elif opr == "-":
+                c = n1 - n2
+            elif opr == "/":
+                if n2 != 0:
+                    c = n1 / n2
+                else:
+                    c = "Error: Division by zero"
+            elif opr == "*":
+                c = n1 * n2
+            else:
+                c = "Invalid operation"
+        else:
+            c = "Error: Please enter valid numbers."
+    print(n1,opr,n2,"=",c)
+    return render(request, "calculator.html", {"c": c})
